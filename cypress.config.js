a79ad0b5bfbd1963a6db20cb3568ee217a5277b7
lrocon18 <lucas.piovezan8@gmail.com>
2023-07-11 18:11:58 -0300

    Atualiza dependencias e workflow

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
     "video": false,
     "reporter": "mochawesome",
     "reporterOptions": {
+      "reportDir": "cypress/mochawesome-report",
       "overwrite": false,
       "html": false,
       "json": true
